1b11bc8470c4a62f14accad585f3a15a255e35a3
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-10 10:48:05 -0500

    using chakra components and error handler

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button, FormErrorMessage, Input, FormControl, Box, Text, Center,Link, Heading } from "@chakra-ui/react"
+//import Link from "next/link"
 import { useState } from "react"
 
 export default function Register() {
@@ -7,10 +9,11 @@ export default function Register() {
     const [email, setEmail] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [error, setError] = useState(null)
+    const [error, setError] = useState('')
 
     const handleSignUp = async() => {
         try {
+
             const res = await fetch("http://localhost:3000/blog-api/register", {
                 method: 'POST',
                 headers: {
@@ -24,10 +27,10 @@ export default function Register() {
             })
 
             if(res.ok) {
-
+                //its ok
             } else {
                 const errorData = await res.json()
-                setError(errorData)
+                setError(errorData.message)
             }
             
 
@@ -38,35 +41,55 @@ export default function Register() {
     }
 
     return (
-        <div>
-            <h1>sign up form</h1>
+        <Box margin={"auto"}
+        maxWidth={960}>
 
-            <form >
-                <input placeholder="username" 
+            <Heading as={"h1"} textAlign={"center"} mt={100}>Sign Up </Heading>
+
+            <FormControl isInvalid={error} margin={"auto"} maxWidth={400}>
+                <Input placeholder="username" 
                 type="text"
                 value={username}
                 minLength={4}
                 onChange={(e) => setUsername(e.target.value)} 
-                maxLength={40} />
+                maxLength={40}
+                mt={5}/>
 
-                <input placeholder="email@youremail.com"
+                <Input placeholder="email@youremail.com"
                 value={email}
-                min={10}
+                minLength={5}
+                mt={5}
                 onChange={(e) => setEmail(e.target.value)}
                 type="email" />
 
-                <input placeholder="password"
+                <Input placeholder="password"
                 value={password}
                 minLength={5}
+                mt={5}
                 onChange={(e) => setPassword(e.target.value)}
                 type="password" />
 
-                <button
-                onClick={handleSignUp}>Sign up</button>
+                {error && (
+                    <FormErrorMessage>
+                        {error}
+                    </FormErrorMessage>
+                )}
 
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-            </form>
-        </div>
+                <Center flexDir={"column"}>
+                    <Button
+                        mt={10}
+                        onClick={handleSignUp}>Sign up</Button>
+                    <Text 
+                    mt={5}
+                    as={"p"}>Do you have an account? <Link href={"/login"} color={"blue"}>log in</Link></Text>
+
+                </Center>
+                
+
+            
+                
+            </FormControl>
+        </Box>
     )
 
 }
