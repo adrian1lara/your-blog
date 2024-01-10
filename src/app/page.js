07d22d9ad7bae8aeb5d261a9b1b6934a6da7f422
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-10 10:46:40 -0500

    added component Posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+'use client'
+
+import Posts from "./components/posts";
 
 export default function Home() {
     return (
         <div>
             <h1>Hello</h1>
+            <Posts/>
         </div>
     )
 }
