6867ce6bbc9321ba80ed0ec50a0ef355e13f8ebc
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-13 15:06:29 -0500

    added chakra ui components and styles

--- src/app/components/posts.js
+++ src/app/components/posts.js
@@ -1,7 +1,7 @@
 
-import { Heading, Box, Text, Link } from "@chakra-ui/react";
+import { Heading, Box, Text, Link, Divider, textDecoration, Center, Flex, ButtonGroup, Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import Comments from "./comments";
+
 
 export default function Posts() {
     
@@ -35,16 +35,23 @@ export default function Posts() {
             <Heading>Posts</Heading>
 
             {post ? (
-                <Box>
+                <Box  width={"100%"} margin={"auto"}>
                 {post.map((post) => (
 
-                    <Link key={post._id} href={`/posts/${post._id}`}>
-                        <Box>
-                            <Text>{post.user.username}</Text>
-                            <Heading as={"h2"}>{post.title}</Heading>
-                            <Text>{post.content}</Text>
-                            <Comments postId={post._id}/>
+                    <Link key={post._id} href={`/posts/${post._id}`} 
+                    _hover={{textDecoration: "none"}}>
+                        <Box p={4} _hover={{ bg: "#33464C"}} borderRadius={5} margin={1}>
+                            <Flex >
+                            <Text color={"#CBD5E0"} _hover={{color: "#2C7A7B"}}>{post.user.username}  
+                            </Text><Text mr={1} ml={1} color={"#CBD5E0"}> • </Text>
+                            <Text as={"span"} color={"#CBD5E0"}> {post.latest}</Text>
+                            </Flex>
+                            
+                            <Heading as='h2' size={"md"} color={"white"}>{post.title}</Heading>
+                            <Text textColor={"GrayText"}>{post.content}</Text>
+
                         </Box>
+                        <Divider />
                     </Link>
 
                     
