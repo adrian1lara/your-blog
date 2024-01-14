5b60ed8daa7c6ced00332fc82640b6dcfe41081f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-14 01:08:33 -0500

    added OutNav component setting style to the box

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 
 import { Box } from "@chakra-ui/react";
 import Posts from "./components/posts";
+import OutNav from "./components/noUserNav";
 
 export default function Home() {
     return (
         <Box>
-            <Box margin={"auto"} border={"1px solid"}>
+            <OutNav />
+            <Box margin={"auto"} mt={"6%"} maxWidth={"960px"} border={"1px solid"}>
                 <Posts />
             </Box>
         </Box>
