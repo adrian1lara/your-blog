d8db54c8d83d9cb18dfba217c082085ef7daf39f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-16 17:01:17 -0500

    hadle if user is logged or not to display the correct nav

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,20 @@
 import { Box } from "@chakra-ui/react";
 import Posts from "./components/posts";
 import OutNav from "./components/noUserNav";
+import LogNav from "./components/userNav";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [accessToken, setAccessToken] = useState('');
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        setAccessToken(token);
+      }, []);
+
     return (
         <Box>
-            <OutNav />
+            {accessToken ? <LogNav /> : <OutNav />}
             <Box margin={"auto"} mt={"10%"} maxWidth={"960px"} border={"1px solid"}>
                 <Posts />
             </Box>
