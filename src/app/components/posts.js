4944737b796d62868d4e1f6784bbe087470cb1e5
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-16 00:32:20 -0500

    added avatar to my posts

--- src/app/components/posts.js
+++ src/app/components/posts.js
@@ -1,5 +1,5 @@
 
-import { Heading, Box, Text, Link, Divider, textDecoration, Center, Flex, ButtonGroup, Button } from "@chakra-ui/react";
+import { Heading, Box, Text, Link, Divider, textDecoration, Center, Flex, ButtonGroup, Button, Avatar } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 
@@ -41,13 +41,14 @@ export default function Posts() {
                     <Link key={post._id} href={`/posts/${post._id}`} 
                     _hover={{textDecoration: "none"}}>
                         <Box p={4} _hover={{ bg: "#33464C"}} borderRadius={5} margin={1}>
-                            <Flex >
+                            <Flex  alignItems={"center"}>
+                            <Avatar mr={2} size={"sm"} src="https://i.pinimg.com/736x/92/26/d7/9226d738bb7e00aa1bff0b73b786ae00.jpg"/>
                             <Text color={"#CBD5E0"} _hover={{color: "#2C7A7B"}}>{post.user.username}  
                             </Text><Text mr={1} ml={1} color={"#CBD5E0"}> • </Text>
                             <Text as={"span"} color={"#CBD5E0"}> {post.latest} ago</Text>
                             </Flex>
                             
-                            <Heading as='h2' size={"md"} color={"white"}>{post.title}</Heading>
+                            <Heading as='h2' size={"md"} color={"white"} mt={2}>{post.title}</Heading>
                             <Text textColor={"GrayText"}>{post.content}</Text>
 
                         </Box>
